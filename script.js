af697688929935469b64cada978998bc2f438506
Shikhar Gupta <69209995+shiikharrr@users.noreply.github.com>
2026-04-20 13:28:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-document.getElementById("sum_btn").addEventListener("click", () => {
-  const prices = document.querySelectorAll(".prices");
-  let total = 0;
+const prices = document.querySelectorAll(".prices");
+let total = 0;
 
-  prices.forEach((price) => {
-    total += parseInt(price.innerText.trim(), 10);
-  });
-
-  document.getElementById("ans").innerText = total;
+prices.forEach(price => {
+  total += parseInt(price.textContent.trim(), 10);
 });
+
+document.getElementById("ans").textContent = total;
