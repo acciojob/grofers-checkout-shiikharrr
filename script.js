ad6ba81277b5571d4230656d77a767972da117ed
Shikhar Gupta <69209995+shiikharrr@users.noreply.github.com>
2026-04-20 13:11:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,29 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+  const prices = document.querySelectorAll(".prices");
+  const table = document.querySelector("table");
+
+  let total = 0;
+
+  prices.forEach((price) => {
+    total += Number(price.textContent);
+  });
+
+  const oldTotalRow = document.getElementById("total-row");
+  if (oldTotalRow) {
+    oldTotalRow.remove();
+  }
+
+  const tr = document.createElement("tr");
+  tr.id = "total-row";
+
+  const td = document.createElement("td");
+  td.setAttribute("colspan", "2");
+  td.textContent = total;
+
+  tr.appendChild(td);
+  table.appendChild(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
